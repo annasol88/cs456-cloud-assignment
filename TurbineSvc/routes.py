59ce434d1f694ca984e5522bdba6f0514e287aaf
annasol88 <aniamonika@yahoo.com>
2024-03-15 10:30:47 +0000

    wip

--- TurbineSvc/routes.py
+++ TurbineSvc/routes.py
@@ -1,7 +1,6 @@
-from http.client import UNSUPPORTED_MEDIA_TYPE
 from flask import Blueprint, request, jsonify,make_response
 from models import db
-import models
+import models as models
 
 turbines_bp = Blueprint('turbines', __name__, url_prefix='/turbines')
 measurements_bp = Blueprint('measurements', __name__, url_prefix='/measurements')
@@ -17,6 +16,7 @@ def turbines():
                 turbine = models.Turbine(serial = data['serial'])
                 db.session.add(turbine)
                 db.session.commit()
+                return jsonify(turbine.to_dict()['id'])
         except TypeError:
             return make_response(jsonify("No JSON supplied."), 500)
         except KeyError:
@@ -30,9 +30,11 @@ def turbines():
 def turbine(id):
     turbine = models.Turbine.query.filter_by(id=id).first()
     if turbine is None:
-        return make_response(jsonify("Index not found."), 500)
+        return make_response(jsonify("Index not found."), 200)
+    
     if request.method == 'GET':
         return jsonify(turbine.to_dict())
+    
     if request.method == 'DELETE':
         try:
             db.session.delete(turbine)
@@ -40,6 +42,7 @@ def turbine(id):
             return make_response(jsonify({}), 200)
         except Exception:
             return make_response(jsonify("Internal server error."), 500)
+        
     return make_response(jsonify("Method not supported."), 404)
 
 
@@ -47,13 +50,13 @@ def turbine(id):
 def measurement(id):
     if request.method == 'GET':
         serial = request.args.get('serial')
-        measurements = None
         if serial is None:
             measurements = models.Measurement.query.all()
+            return jsonify(measurements.to_dict())
             
-        measurements = models.Measurement.query.filter_by(serial=serial).first()
-        
-        if measurements is None and serial is None:
-            return make_response(jsonify("Measurement serial not found."), 500)
-        return jsonify(measurements.to_dict())
+        measurement = models.Measurement.query.filter_by(serial=serial).first()
+        if measurement is None:
+            return make_response(jsonify("Measurement with serial number not found."), 200)
+        return jsonify(measurement.to_dict())
+    
     return make_response(jsonify("Method not supported."), 404)
